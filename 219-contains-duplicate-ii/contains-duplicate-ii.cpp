7bf1c8da71dbd41731abc65c144d50d2aba75fe0
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-07-10 14:00:08 +0530

    Time: 127 ms (17.20%) | Memory: 102.5 MB (6.62%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        set<int>window;
+        for(int i=0;i<nums.size();i++){
+
+            if(i>k){
+                window.erase(nums[i-k-1]);
+            }
+            
+            if(window.count(nums[i])){
+                return true;
+            }
+            
+                window.insert(nums[i]);         
+            
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
+
+/*class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
             if(mpp.count(nums[i])&& i-mpp[nums[i]]<=k ){
@@ -10,4 +38,4 @@ public:
         }
         return false;
     }
-};
+};*/
