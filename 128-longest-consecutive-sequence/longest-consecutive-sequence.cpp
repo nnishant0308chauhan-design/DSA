50d919364e49b92286a3df690eeae185699106ad
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-07-28 18:48:14 +0530

    Time: 88 ms (26.74%) | Memory: 89 MB (25.71%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        int ans=0;
+        int count;
+        for(int x:st){
+            int curr;
+            if(st.count(x-1)==0){
+                 curr=x;
+                  count=1;
+
+                  while(st.count(curr+1)){
+                        count++;
+                        curr++;
+                  }
+                   ans=max(ans,count);
+            }
+           
+           
+        }
+        return ans;
+    }
+};
+
+
+
+
+/*class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int count=1;
         int m=1;
@@ -22,4 +53,4 @@ public:
         }
         return m;
     }
-};
+};*/
