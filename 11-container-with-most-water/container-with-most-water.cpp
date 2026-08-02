907e8c343bc47565ab7ec50a35b4c74eb28f6de2
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-02 11:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (11.29%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,47 +4,19 @@ public:
         int i=0;
         int j=height.size()-1;
         int area;
-        int max_water=0;
+        int ans=0;
+
         while(i<j){
-             area=min(height[i],height[j]) * (j-i);
-             
-            
+            area=min(height[i],height[j]) *(j-i);
+            ans=max(ans,area);
+
             if(height[i]<height[j]){
                 i++;
             }else{
                 j--;
             }
-             max_water=max(max_water,area);
-           
         }
-        return max_water;
+        return ans;
+
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-//this soln logic is correct but gives TLE because of O(N^2)
-
-/*class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int area=0;
-        int max_water=INT_MIN;
-        for(int i=0;i<height.size();i++){
-            for(int j=i+1;j<height.size();j++){
-                 area=min(height[i],height[j]) * (j-i);
-                 max_water=max(max_water,area);
-            }
-            
-        }
-        return max_water;
-    }
-};*/
