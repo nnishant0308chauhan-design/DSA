586ed13f021d647d8418e3e9ac1c072bfa01035d
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-04 17:20:16 +0530

    Time: 9 ms (29.49%) | Memory: 13.1 MB (90.81%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int count =0;
-        int ans;
-
+        int count=0;
+        int ans=0;
         for(int i=0;i<k;i++){
             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
                 count++;
             }
-        }
-        ans=count;
+        }   
+            ans=count;
+            for(int i=k;i<s.size();i++){
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                    count++;
+                }
+                if(s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u'){
+                    count--;
+                }
+                ans=max(ans,count);
+            }
         
-        for(int i=k;i<s.size();i++){
-            if(s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u'){
-                count--;
-            }
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-                count++;
-            }
-            ans=max(count,ans);
-        }
         return ans;
     }
 };
