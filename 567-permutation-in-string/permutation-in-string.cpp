1d9aa2ac90083c9dac028b683364da755d3474fe
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-06 17:56:51 +0530

    Time: 7 ms (39.39%) | Memory: 10.9 MB (20.64%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int>mpp;
-        unordered_map<char,int>mpp2;
- 
-        if(s1.size()>s2.size()){
+        unordered_map<char, int>mpp1;
+         unordered_map<char, int>mpp2;
+
+         if(s1.size()>s2.size()){
             return false;
-        }
-        for(auto x: s1){
-            mpp[x]++;
-        }
-        int left=0;
-        for(int i=0;i<s2.size();i++){
-            mpp2[s2[i]]++;
+         }
+
+         for(auto x:s1){
+            mpp1[x]++;
+         }
+         int left=0;
+         for(int i=0;i<s2.size();i++){
             if(i-left+1>s1.size()){
                 mpp2[s2[left]]--;
                 if(mpp2[s2[left]]==0){
                     mpp2.erase(s2[left]);
                 }
-                left++;
+               left++; 
             }
-
-            if(i-left+1==s1.size() && mpp==mpp2){
+            mpp2[s2[i]]++;
+            if(i-left+1==s1.size() &&mpp1==mpp2){
                 return true;
             }
-        }
-        return false;
+         }
+         return false;
     }
 };
