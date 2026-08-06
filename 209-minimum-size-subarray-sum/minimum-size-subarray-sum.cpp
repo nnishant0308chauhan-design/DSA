01ce34fc46710f933cf9aa4b4278bc60364915f9
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-06 17:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (79.97%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -5,13 +5,14 @@ public:
         int left=0;
         int ans=INT_MAX;
         for(int i=0;i<nums.size();i++){
-             sum+=nums[i];
-                
-             while(sum>=target){
-                   ans=min(ans,i-left+1);
-                   sum-=nums[left];
-                   left++;
-             }
+           sum+=nums[i];
+           
+           while(sum>=target){
+               ans=min(ans,i-left+1);
+               sum-=nums[left];
+               left++;
+           }
+
         }
         if(ans==INT_MAX) return 0;
         return ans;
