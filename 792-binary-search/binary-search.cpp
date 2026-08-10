64571fe17912fe619eb262e4955ce057aa38e2ab
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-10 17:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (47.00%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,8 +3,10 @@ public:
     int search(vector<int>& nums, int target) {
         int left=0;
         int right=nums.size()-1;
+
         while(left<=right){
             int mid=(left+right)/2;
+
             if(nums[mid]==target){
                 return mid;
             }
