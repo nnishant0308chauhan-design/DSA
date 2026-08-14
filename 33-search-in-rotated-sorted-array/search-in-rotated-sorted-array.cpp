4a67757ad8ed7f2963393e621b26b3170eb56ab6
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-14 09:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (69.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,20 +5,20 @@ public:
         int right=nums.size()-1;
 
         while(left<=right){
-            
             int mid=(left+right)/2;
-
-            if(nums[mid]==target){
+             
+             if(nums[mid]==target){
                 return mid;
-            }
+             }
+
             if(nums[left]<=nums[mid]){
-                if(target<=nums[mid] && target>=nums[left]){
+                if(nums[left]<=target&&nums[mid]>=target){
                     right=mid-1;
                 }else{
                     left=mid+1;
                 }
             }else{
-                if(target >=nums[mid] && target<=nums[right]){
+                if(nums[mid]<=target&&nums[right]>=target){
                     left=mid+1;
                 }else{
                     right=mid-1;
