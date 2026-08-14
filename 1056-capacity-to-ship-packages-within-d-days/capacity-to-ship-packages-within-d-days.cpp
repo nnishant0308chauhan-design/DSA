f03bb615e8dd2b0a7e5cdc13005211acdde0fb34
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-14 13:36:15 +0530

    Time: 16 ms (19.41%) | Memory: 35.1 MB (20.16%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int total=0;
-        int mini=INT_MIN;
-        for(int i=0;i<weights.size();i++){
-            total+=weights[i];
-            mini=max(mini,weights[i]);
+        int sum=0;
+        int m=0;
+        for(int x:weights){
+            sum+=x;
+            m=max(m,x);
         }
-        int left=mini;
-        int right=total;
+
+        int left=m;
+        int right=sum;
         while(left<=right){
             int mid=(left+right)/2;
-            int reqd=1;
+            int day=1;
             int curr=0;
-            for(int w:weights){
-                if(curr+w>mid){
-                    reqd++;
+            for(int x:weights){
+                if(curr+x >mid){
+                    day++;
                     curr=0;
                 }
-                curr+=w;
+                curr+=x;
             }
-
-            if(reqd<=days){
+            if(day<=days){
                 right=mid-1;
             }else{
                 left=mid+1;
