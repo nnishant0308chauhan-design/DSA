8bda6a5aff3effcabb2a665204c4031ac0b3ac47
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-08-18 21:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (11.93%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,10 +6,11 @@ public:
 
         while(left<right){
             int mid=(left+right)/2;
-            if(nums[mid]>nums[right]){
-                left=mid+1;
-            }else{
+
+            if(nums[mid]<nums[mid+1] && nums[mid]<nums[right]){
                 right=mid;
+            }else{
+               left=mid+1;
             }
         }
         return nums[left];
