bea70ec0bbb847375dc205d666aee106f59d8bdc
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-09-13 15:49:27 +0530

    Time: 4 ms (1.50%) | Memory: 33.3 MB (30.24%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,6 +1,53 @@
 class Solution {
 public:
+
+    vector<vector<int>> dp;
+
+    bool solve(vector<int>& nums, int i, int target) {
+
+        if (i == nums.size())
+            return true;
+
+        if (dp[i][target] != -1)
+            return dp[i][target];
+
+        // Keep nums[i]
+        if ((nums[i] & 1) == target) {
+            if (solve(nums, i + 1, target))
+                return dp[i][target] = true;
+        }
+
+        // Subtract another element
+        for (int j = 0; j < nums.size(); j++) {
+
+            if (j == i)
+                continue;
+
+            int value = nums[i] - nums[j];
+
+            if ((value & 1) == target) {
+                if (solve(nums, i + 1, target))
+                    return dp[i][target] = true;
+            }
+        }
+
+        return dp[i][target] = false;
+    }
+
     bool uniformArray(vector<int>& nums1) {
-        return 1;
+
+        int n = nums1.size();
+
+        dp.assign(n, vector<int>(2, -1));
+
+        if (solve(nums1, 0, 0))
+            return true;
+
+        dp.assign(n, vector<int>(2, -1));
+
+        if (solve(nums1, 0, 1))
+            return true;
+
+        return false;
     }
 };
