38063f0206e3e3421481eea326803b2061a78f25
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-09-17 12:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (35.49%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int>ans;
+        
+
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                ans.push_back(i);
+            }
+        }
+
+        if(k>ans.size()){
+        return -1;
+      }
+      
+      int answer=ans[k-1];
+     return answer;
+    }
+};
