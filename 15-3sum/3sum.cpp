d7fbabbe176c0af2ad2bf1212d85799f4f85367d
nnishant0308chauhan-design <nnishant0308chauhan@gmail.com>
2026-09-20 12:31:11 +0530

    Time: 43 ms (82.77%) | Memory: 29.1 MB (72.94%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,22 +5,23 @@ public:
         vector<vector<int>>ans;
 
         for(int i=0;i<nums.size();i++){
-             if(i>0 && nums[i]==nums[i-1]){
+            if(i>0&&nums[i]==nums[i-1]){
                 continue;
-             }
-             int a=i+1;
-             int b=nums.size()-1;
+            }
+            int a=i+1;
+            int b=nums.size()-1;
+
             while(a<b){
                 int sum=nums[i]+nums[a]+nums[b];
-                
+
                 if(sum==0){
                     ans.push_back({nums[i],nums[a],nums[b]});
                     a++;
                     b--;
-                    while(a<b && nums[a]==nums[a-1]){
+                    while(a<b&&nums[a]==nums[a-1]){
                         a++;
                     }
-                    while(a<b && nums[b]==nums[b+1]){
+                    while(a<b&&nums[b]==nums[b+1]){
                         b--;
                     }
                 }
